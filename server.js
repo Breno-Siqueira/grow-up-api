f6dd8adfbe9e0a085cc1f217fd0c9d3d426497b8
Breno Siqueira <breno.twenty7@gmail.com>
2022-05-18 18:45:39 -0300

    criando outra rota

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!!');
 });
 
+app.get('/teste', (req, res) => {
+  res.send('teste de rota!!');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
