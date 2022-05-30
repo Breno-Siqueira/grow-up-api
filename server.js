917944ed8ba2d90805b5691a1391cce7525c87fb
Rodrigues <breno.rodrigues@capgemini.com>
2022-05-30 10:47:39 -0300

    alterando porta

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const knex = require('knex');
 const app = express();
-const port = 3333;
+const port = 8080;
 // const database = require('./db');
 const User = require('./User');
 
