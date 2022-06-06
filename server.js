7e85f1fde7fe00a3af31843e73d4cbe808211132
Breno Siqueira <breno.twenty7@gmail.com>
2022-06-06 18:25:41 -0300

    corrigindo cors

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   //Quais são os métodos que a conexão pode realizar na API
   res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-  app.use(cors());
   next();
 });
+app.use(cors());
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
